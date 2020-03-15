0b3f23437a488e9e20a86618f2c935ca0bd998ff
Louis Coleman <coleloui18@gmail.com>
2020-03-15 13:06:25 -0700

    styling and some quesitons

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var questionEl = document.getElementById("question")
 var answersEl = document.querySelectorAll(".answer")
 var answer1El = document.getElementById("answer1")
 var answer2El = document.getElementById("answer2")
+var answer3El = document.getElementById("answer3")
+var answer4El = document.getElementById("answer4")
 var timerInterval
 var counter = 0
 var correct = 0
@@ -14,13 +16,38 @@ var incorrect = 0
 
 questArray = [{
     question: "Who was the first player drafted by the Lightning",
-    answer1: "Roman Hamerlick",
+    answer1: "Roman Hamrlik",
     answer2: "John Snow",
+    answer3: "Dino Ciccarelli",
+    answer4: "Chris Gratton",
     correct: "Roman Hamerlick"
 },{
     question: "Where did the Lightining first play",
-    answer1: "Florida State Fairgrounds",
+    answer1: "Amalie Arena",
     answer2: "Thunderdome",
+    answer3: "Raymond James Stadium",
+    answer4: "Florida State Fairgrounds",
+    correct: "Florida State Fairgrounds"
+},{
+    question: "What year did the Lightning win the Stanley Cup",
+    answer1: "They have yet to win the best trophy in professions sports",
+    answer2: "1999",
+    answer3: "2004",
+    answer4: "2014",
+    correct: "2004"
+},{
+    question: "Where did the Lightining first play",
+    answer1: "Amalie Arena",
+    answer2: "Thunderdome",
+    answer3: "Raymond James Stadium",
+    answer4: "Florida State Fairgrounds",
+    correct: "Florida State Fairgrounds"
+},{
+    question: "Where did the Lightining first play",
+    answer1: "Amalie Arena",
+    answer2: "Thunderdome",
+    answer3: "Raymond James Stadium",
+    answer4: "Florida State Fairgrounds",
     correct: "Florida State Fairgrounds"
 }]
 
@@ -29,8 +56,9 @@ function renderQuestions() {
     questionEl.textContent = questArray[counter].question
     answer1El.textContent = questArray[counter].answer1
     answer2El.textContent = questArray[counter].answer2
+    answer3El.textContent = questArray[counter].answer3
+    answer4El.textContent = questArray[counter].answer4
     timer()
-    // console.log(answersEl)
 }
 
 
@@ -60,18 +88,17 @@ function handleCorrect(){
     console.log("you guessed correct")
     correct++
     counter++
-    if(counter === questArray.length){
-        alert("game over")
-    }else{
-        renderQuestions()
-        // end screen
-    }
+    checker()
 }
 
 function handleIncorrect(){
     console.log("you guessed incorrect")
     incorrect++
     counter++
+    checker()
+}
+
+function checker(){
     if(counter === questArray.length){
         alert("game over")
     }else{
